ebb0ebc54a3bd2836918b24ad20eda5406b35445
CHAN <58592042+Godfather-Chan@users.noreply.github.com>
2025-12-03 11:46:08 +0900

    Delete frontend directory

--- frontend/lib/api/analysis.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "./axios"
-import type { AnalysisInput, AnalysisResult } from "../types"
-
-export const analysisApi = {
-  async analyzeUser(input: AnalysisInput): Promise<AnalysisResult> {
-    const response = await apiClient.post<AnalysisResult>("/analysis", input)
-    return response.data
-  },
-}
